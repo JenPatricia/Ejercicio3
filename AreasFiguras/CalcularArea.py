e4225a4db4bf69e49245e03f038d75a7cecc1a99
1-21-01-22B-09\pc1-21-01-22B-03 <jorge.iza@epn.edu.ec>
2017-11-30 09:43:44 -0500

    se implemento el area del triangulo

--- AreasFiguras/CalcularArea.py
+++ AreasFiguras/CalcularArea.py
@@ -10,3 +10,11 @@ lado2 = int (input("Ingrese lado2: "))
 areaCu = calcularAreaCuadrado(lado1,lado2)
 print("La area del cuadrado es: "+str(areaCu))
 
+def calcularAreaTriangulo(b,a):
+    areaTriangulo = (b * a) / 2
+    return areaTriangulo
+
+base = int (input("Ingrese la base del triangulo en metros:\n"))
+altura = int (input("Ingrese la altura del triangulo en metros:\n"))
+areaTriangulo = calcularAreaTriangulo(base,altura)
+print("El area del triangulo es: "+str())
