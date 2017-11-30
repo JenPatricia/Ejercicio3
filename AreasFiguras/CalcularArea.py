9da86f98bb2a2a0bebcaa20ba942388deef6f1f0
1-21-01-22B-15\pc1-21-01-22B-03 <jenny.tipan@epn.edu.ec>
2017-11-30 09:55:37 -0500

    Parte area rectangulo

--- AreasFiguras/CalcularArea.py
+++ AreasFiguras/CalcularArea.py
@@ -3,12 +3,17 @@
 def calcularAreaCuadrado(l1,l2):
     area = l1 * l2
     return area
-
-
 lado1 = int(input("Ingrese lado1: "))
 lado2 = int (input("Ingrese lado2: "))
 areaCu = calcularAreaCuadrado(lado1,lado2)
 print("La area del cuadrado es: "+str(areaCu))
+
+def area_Rectangulo():
+    print('AREA RECTANGULO')
+    base = float(input('Ingrese valor de la base del rectangulo'))
+    altura = float(input('Ingrese valor de la altura del rectangulo'))
+    area = base * altura
+    print('El area del rectangulo es: ' + str(round(area, 2)))
 
 def calcularAreaTriangulo(b,a):
     areaTriangulo = (b * a) / 2
@@ -18,3 +23,5 @@ base = int (input("Ingrese la base del triangulo en metros:\n"))
 altura = int (input("Ingrese la altura del triangulo en metros:\n"))
 areaTriangulo = calcularAreaTriangulo(base,altura)
 print("El area del triangulo es: "+str())
+
+area_Rectangulo()
